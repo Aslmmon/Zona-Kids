d8a50806a67f02671d0f6961fb3d942ec8ca1ae9
Aslm <aslmmon993@gmail.com>
2024-10-14 20:49:35 -0300

    * added Skip Button

--- lib/ui/registration/login/LoginScreen.dart
+++ lib/ui/registration/login/LoginScreen.dart
@@ -9,6 +9,23 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
+      backgroundColor: Theme.of(context).primaryColorLight,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 40.0),
+            child: AppButton(
+              AppButtonText: 'Skip',
+              onAppButtonClick: () {},
+              width: 70,
+              AppButtonColor: Theme.of(context).primaryColorLight,
+            ),
+          )
+        ],
+        backgroundColor: Theme.of(context).primaryColorLight,
+      ),
       body: Stack(
         fit: StackFit.expand,
         children: [
@@ -16,38 +33,42 @@ class LoginScreen extends StatelessWidget {
             'assets/register_bg.svg',
             fit: BoxFit.cover,
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                'Login Now',
-                style: TextStyle(
-                    fontSize: 24, color: Theme.of(context).primaryColorLight),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: EditText(hintText: 'Email'),
-              ),
-              EditText(hintText: 'Password'),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: AppButton(
-                  width: 120,
-                  AppButtonText: 'Login',
-                  onAppButtonClick: () {},
-                  AppButtonColor: Theme.of(context).primaryColorLight,
-                ),
-              ),
-              GestureDetector(
-                onTap: () {},
-                child: Text(
-                  'Create New User',
+          Padding(
+            padding: const EdgeInsets.only(bottom: 10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Login Now',
                   style: TextStyle(
                       fontSize: 24, color: Theme.of(context).primaryColorLight),
                 ),
-              )
-            ],
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: EditText(hintText: 'Email'),
+                ),
+                EditText(hintText: 'Password'),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: AppButton(
+                    width: 120,
+                    AppButtonText: 'Login',
+                    onAppButtonClick: () {},
+                    AppButtonColor: Theme.of(context).primaryColorLight,
+                  ),
+                ),
+                GestureDetector(
+                  onTap: () {},
+                  child: Text(
+                    'Create New User',
+                    style: TextStyle(
+                        fontSize: 24,
+                        color: Theme.of(context).primaryColorLight),
+                  ),
+                )
+              ],
+            ),
           ),
         ],
       ),
